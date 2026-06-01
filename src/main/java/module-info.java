73fca42dbb96b17652806ec0a603a546cca8aaa1
Gabs Melo <gabsmelo19.dev@gmail.com>
2026-06-01 09:12:15 -0300

    Resolvendo bugs: não era possível executar a aplicação, pois o arquivo HelloController não foi encontrado. Agora o caminho dele foi atualizado dentro do main.fxml.
    Liberado o acesso para o JavaFX conseguir manipular arquivos controllers em "com.cardgame.ui.controller"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.cardgame.cardgame {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
-
+    opens com.cardgame.ui.controller to javafx.fxml;
     opens com.cardgame.cardgame to javafx.fxml;
     exports com.cardgame.cardgame;
+    exports com.cardgame.ui.controller;
 }
