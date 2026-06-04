f46572a6f99d80a19d44b1e19b5110d02b8bcadb
Gabs Melo <gabsmelo19.dev@gmail.com>
2026-06-04 13:58:58 -0300

    Atualizando Arquivo: removendo elemento "neutro" e adicionando elementos (planta, eletrico, gelo e metal)

--- src/main/java/com/cardgame/model/Elemento.java
+++ src/main/java/com/cardgame/model/Elemento.java
@@ -3,9 +3,12 @@ package com.cardgame.model;
 public enum Elemento {
     FOGO,
     AGUA,
+    PLANTA,
     TERRA,
+    ELETRICO,
+    GELO,
+    METAL,
     AR,
     LUZ,
-    TREVAS,
-    NEUTRO
+    TREVAS
 }
